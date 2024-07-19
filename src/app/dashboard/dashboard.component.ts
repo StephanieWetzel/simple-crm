682701b5d0bd0eb835ad544c5c9b8983cdadb3e9
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-07-19 20:49:28 +0200

    implemented chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  title = 'donations';
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+  };
+
+  public pieChartLabels: string[] = ['Individuals', 'Corporations', 'Foundations', 'Government Agencies'];
+  public pieChartDatasets = [{
+    data: [72, 8, 15, 5],
+    backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0']
+  }];
+  public pieChartLegend = true;
+  public pieChartPlugins = [
+
+  ];
+
+
+  constructor() {
+  }
 
 }
